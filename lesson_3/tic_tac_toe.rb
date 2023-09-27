dce85508d7171d93a19419dcc1876d044d024506
Hugo Ma <huwgoma@gmail.com>
2023-09-27 16:39:51 -0400

    RB110 TTT - Refactor Game Loop

--- lesson_3/tic_tac_toe.rb
+++ lesson_3/tic_tac_toe.rb
@@ -9,6 +9,11 @@ def prompt(str)
   puts ">> #{str}"
 end
 
+def display_rules
+  prompt("Welcome to Tic Tac Toe!")
+  prompt("You will be playing a best-of-9 series against the computer. First to 5 wins!")
+end
+
 def prompt_name
   prompt("Hello! What's your name?")
   loop do
@@ -51,7 +56,7 @@ def display_game(board, scores)
 end
 
 def display_scores(scores)
-  prompt("Player (#{PLAYER_MARKER}): #{scores[:player]}; CPU (#{CPU_MARKER}): #{scores[:cpu]}")
+  prompt("Player (#{PLAYER_MARKER}): #{scores['player']}; CPU (#{CPU_MARKER}): #{scores['cpu']}")
 end
 
 def display_board(board)
@@ -71,6 +76,11 @@ def display_game_result(winner) # string or nil
   else
     prompt("It's a tie!")
   end
+end
+
+def display_series_result(scores)
+  winner = scores.max[0].capitalize
+  prompt("#{winner} wins with a score of #{scores.max[1]}-#{scores.min[1]}!")
 end
 
 # Board:
@@ -115,7 +125,7 @@ end
 
 def increment_score(scores, winner)
   return if winner.nil?
-  scores[winner.downcase.to_sym] += 1
+  scores[winner.downcase] += 1
 end
 
 # Win-Finding Logic
@@ -193,114 +203,38 @@ def play_again?
 end
 
 # Main Loop
-name = prompt_name
+# name = prompt_name ??
+display_rules
 PLAYER_MARKER, CPU_MARKER = choose_marker
+
 # Series (Bo9)
 loop do
-  scores = { player: 0, cpu: 0 }
+  scores = { 'player' => 0, 'cpu' => 0 }
+
   # Game
   loop do
     board = create_board
     current_marker, next_marker = PLAYER_MARKER, CPU_MARKER
-    # Turn(s)
+
+    # Game Turns
     loop do
       display_game(board, scores)
       place_piece!(board, current_marker)
       break if winner?(board) || tie?(board)
       current_marker, next_marker = next_marker, current_marker
     end
-    winner = find_winner(board) # string or nil (if tie)
+
+    winner = find_winner(board)
     increment_score(scores, winner)
     display_game(board, scores)
     display_game_result(winner)
-    binding.pry
-    # display the game result
+    
+    break if scores.values.any?(5)
+    prompt("Press any key to continue:")
+    STDIN.getch
   end
+  display_series_result(scores)
+
+  break unless play_again?
 end
-
-
-
-# 5) When the game loop is broken: 
-# - Increment the scores accordingly
-# - Display the game board
-# - Display who won the round (/if it was a tie)
-
-# 6) Check if the SERIES loop should be broken: 
-# - Check if either player or cpu score is equal to 5.
-# - If the loop is not broken, press any key to continue => next game of series
-
-# 7) When the SERIES loop is broken:
-# - Determine who the series winner was (should be the current player; alternatively, whoever has the higher score (max by score))
-# - Display the winner and score by which they won
-# - Ask if want play again? => Start new series
-#   - if not: goodbye
-
-
-
-
-
-
-# New Series (Bo9)
-# loop do
-#   scores = { player: 0, cpu: 0 }
-
-#   # New Game
-#   loop do
-#     board = create_board
-
-#     # New Turn
-#     loop do
-#       display_game(board, scores)
-  
-#       player_move(board)
-#       break if winner?(board) || tie?(board)
-#       computer_move(board)
-#       break if winner?(board) || tie?(board)
-#     end
-
-
-#     display_game(board, scores)
-
-#     if winner?(board)
-#       prompt("#{find_winner(board)} wins!")
-#     else
-#       prompt("It's a tie!")
-#     end
-
-    
-    
-#     scores[:player] += 1 if find_winner(board) == 'Player'
-#     scores[:cpu] += 1 if find_winner(board) == 'Computer'
-
-#     break if scores[:player] == 5 || scores[:cpu] == 5
-
-#     prompt("Press any key to continue:")
-#     STDIN.getch
-#   end
-
-#   puts 'Player wins!' if player_wins == 5 # Player wins 5-0!
-#   puts 'Computer wins!' if computer_wins == 5 
-
-#   break unless play_again?
-# end
-
-# puts "Thanks for playing!"
-
-
-
-# FIX GAME LOOP SEQUENCE NEXT.
-# Start program:
-# -ask for name 
-# -ask for marker choice
-# 1) Start a new SERIES (best of 9).
-#   => Reset scores to 0
-#   => Start a new GAME (single game)
-# 2) Create a new board for the new game
-#   - Start the game:
-# 3) Display the board (and scores)
-# - Set the current_marker to PLAYER MARKER
-# - Get the current player's move
-# - Mark the board with the current player's marker (mark_board!)?
-
-# 4) Check for a win/tie - break game loop if there is a win or tie
-#   - if not, switch the current player
+prompt("Thanks for playing. Goodbye!")
--- lesson_3/tic_tac_toe_pedac.rb
+++ lesson_3/tic_tac_toe_pedac.rb
@@ -10,6 +10,91 @@
 # the game ends in a tie.
 
 # Flowchart:
+
+# New Series (Bo9)
+# loop do
+#   scores = { player: 0, cpu: 0 }
+
+#   # New Game
+#   loop do
+#     board = create_board
+
+#     # New Turn
+#     loop do
+#       display_game(board, scores)
+  
+#       player_move(board)
+#       break if winner?(board) || tie?(board)
+#       computer_move(board)
+#       break if winner?(board) || tie?(board)
+#     end
+
+
+#     display_game(board, scores)
+
+#     if winner?(board)
+#       prompt("#{find_winner(board)} wins!")
+#     else
+#       prompt("It's a tie!")
+#     end
+
+    
+    
+#     scores[:player] += 1 if find_winner(board) == 'Player'
+#     scores[:cpu] += 1 if find_winner(board) == 'Computer'
+
+#     break if scores[:player] == 5 || scores[:cpu] == 5
+
+#     prompt("Press any key to continue:")
+#     STDIN.getch
+#   end
+
+#   puts 'Player wins!' if player_wins == 5 # Player wins 5-0!
+#   puts 'Computer wins!' if computer_wins == 5 
+
+#   break unless play_again?
+# end
+
+# puts "Thanks for playing!"
+
+
+
+# FIX GAME LOOP SEQUENCE NEXT.
+# Start program:
+# -ask for name 
+# -ask for marker choice
+# 1) Start a new SERIES (best of 9).
+#   => Reset scores to 0
+#   => Start a new GAME (single game)
+# 2) Create a new board for the new game
+#   - Start the game:
+# 3) Display the board (and scores)
+# - Set the current_marker to PLAYER MARKER
+# - Get the current player's move
+# - Mark the board with the current player's marker (mark_board!)?
+
+# 4) Check for a win/tie - break game loop if there is a win or tie
+#   - if not, switch the current player
+
+
+# 5) When the turn loop is broken: 
+# - Increment the scores accordingly
+# - Display the game board
+# - Display who won the round (/if it was a tie)
+
+# 6) Check if the SERIES (individual games) loop should be broken: 
+# - Check if either player or cpu score is equal to 5.
+# - If the loop is not broken, press any key to continue => next game of series
+
+
+# 7) When the SERIES loop is broken:
+# - Determine who the series winner was (should be the current player; alternatively, whoever has the higher score (max by score))
+# - Display the winner and score by which they won
+# - Ask if want play again? => Start new series
+#   - if not: goodbye
+
+
+# ====================================
 # 1) Display the 3x3 board (empty)
 # 2) Prompt the user to select a square to mark. 
 #     - Validate the selected square (ie. cannot be occupied)
@@ -144,3 +229,4 @@
 #   - +5 and +3 (4x4)
 #   - +6 and +4 (5x5)
 #    Board length +/- 1 from starting cell.
+
